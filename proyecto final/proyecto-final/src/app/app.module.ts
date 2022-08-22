92998493194ebc10b10a06d9e9859988d9a2be1e
pwortley <111379554+pwortley@users.noreply.github.com>
2022-08-22 16:45:12 -0300

    Información personal básica, primera modificación.

--- proyecto final/proyecto-final/src/app/app.module.ts
+++ proyecto final/proyecto-final/src/app/app.module.ts
@@ -7,6 +7,9 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedesocialesComponent } from './components/redesociales/redesociales.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { BannerComponent } from './components/banner/banner.component';
     LogoAPComponent,
     RedesocialesComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
